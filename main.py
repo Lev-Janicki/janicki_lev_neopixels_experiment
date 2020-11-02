b0ef737b0a3c7a92886de564bfaf5d6aff2871e6
Lev-Janicki <72770454+Lev-Janicki@users.noreply.github.com>
2020-11-02 13:54:30 -0500

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-#light.set_brightness(5)
-#light.set_pixel_color(0, light.rgb(255,0,0))
-#light.set_pixel_color(1, light.rgb(0,0,255))
-#light.set_pixel_color(2, light.rgb(0,255,0))
-#light.set_pixel_color(3, light.rgb(255,255,0))
-#light.set_pixel_color(4, light.rgb(255,0,255))
-#light.set_pixel_color(5, light.rgb(0,255,255))
-#light.set_pixel_color(6, light.rgb(255,255,255))
+light.set_brightness(5)
+
+
 def on_forever():
+    for i in range(5):
+        light.set_all(light.rgb(255, 0, 255))
+        pause(1000)
+        light.clear()
+        pause(1000)
     pass
-    light.set_all(light.rgb(255,0,255))
-    pause(2000)
-    light.clear()
-    pause(2000)
-    
+    for i in range(5):
+        light.set_pixel_color(0, light.rgb(255,0,0))
+        light.set_pixel_color(1, light.rgb(0,0,255))
+        light.set_pixel_color(2, light.rgb(0,255,0))
+        light.set_pixel_color(3, light.rgb(255,255,0))
+        light.set_pixel_color(4, light.rgb(255,0,255))
+        light.set_pixel_color(5, light.rgb(0,255,255))
+        light.set_pixel_color(6, light.rgb(255,255,255))
+        light.set_pixel_color(7, light.rgb(128,0,255))
+        light.set_pixel_color(8, light.rgb(0,255,128))
+        light.set_pixel_color(9, light.rgb(255,128,0))
+        pause(1000)
+        light.clear()
+        pause(1000)
+    pass
 forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,15 +1,27 @@
-// light.set_brightness(5)
-// light.set_pixel_color(0, light.rgb(255,0,0))
-// light.set_pixel_color(1, light.rgb(0,0,255))
-// light.set_pixel_color(2, light.rgb(0,255,0))
-// light.set_pixel_color(3, light.rgb(255,255,0))
-// light.set_pixel_color(4, light.rgb(255,0,255))
-// light.set_pixel_color(5, light.rgb(0,255,255))
-// light.set_pixel_color(6, light.rgb(255,255,255))
+light.setBrightness(5)
 forever(function on_forever() {
+    let i: number;
+    for (i = 0; i < 5; i++) {
+        light.setAll(light.rgb(255, 0, 255))
+        pause(1000)
+        light.clear()
+        pause(1000)
+    }
     
-    light.setAll(light.rgb(255, 0, 255))
-    pause(2000)
-    light.clear()
-    pause(2000)
+    for (i = 0; i < 5; i++) {
+        light.setPixelColor(0, light.rgb(255, 0, 0))
+        light.setPixelColor(1, light.rgb(0, 0, 255))
+        light.setPixelColor(2, light.rgb(0, 255, 0))
+        light.setPixelColor(3, light.rgb(255, 255, 0))
+        light.setPixelColor(4, light.rgb(255, 0, 255))
+        light.setPixelColor(5, light.rgb(0, 255, 255))
+        light.setPixelColor(6, light.rgb(255, 255, 255))
+        light.setPixelColor(7, light.rgb(128, 0, 255))
+        light.setPixelColor(8, light.rgb(0, 255, 128))
+        light.setPixelColor(9, light.rgb(255, 128, 0))
+        pause(1000)
+        light.clear()
+        pause(1000)
+    }
+    
 })
